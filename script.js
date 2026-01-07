2ad0b8d2dcd4d6ba7d690df975a6ae405459f7e5
Akash Gupta <124385823+Gupta-G-0406@users.noreply.github.com>
2026-01-07 16:49:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+function isLeapYear(year) {
+  if (year % 400 === 0) {
+    return 366;
+  } else if (year % 4 === 0 && year % 100 !== 0) {
+    return 366;
+  } else {
+    return 365;
+  }
+}
+
